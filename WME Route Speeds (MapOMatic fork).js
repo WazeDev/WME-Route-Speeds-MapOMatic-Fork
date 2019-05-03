fdde8f17333b3e1ad797c7becc0a455b05759228
justins83 <justins83.waze@gmail.com>
2019-05-03 14:06:52 -0400

    Update WME Route Speeds (MapOMatic fork).js
    Version bump to pull latest WazeWrap

--- WME Route Speeds (MapOMatic fork).js
+++ WME Route Speeds (MapOMatic fork).js
@@ -2,7 +2,7 @@
 // @name                WME Route Speeds (MapOMatic fork)
 // @description         Shows segment speeds in a route.
 // @include             /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
-// @version             2019.03.08.001
+// @version             2019.05.03.001
 // @grant               none
 // @namespace           https://greasyfork.org/en/scripts/369630-wme-route-speeds-mapomatic-fork
 // @require             https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
